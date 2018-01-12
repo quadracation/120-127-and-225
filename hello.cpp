de758091c592e4f48c023c5d27acf05790470147
quadracation <32320205+quadracation@users.noreply.github.com>
2018-01-12 13:41:04 -0800

    ay lmao

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main (void) {
+	int x;
+    cin>>x;
+    if(x==7){
+    	cout<<"301302662 zbcao";
+    }
+    else if(x==42){
+    	cout<<"Shh!";
+    }
+    else{
+    	cout<<"Hello world!";
+    }
+	return 0;
+}
